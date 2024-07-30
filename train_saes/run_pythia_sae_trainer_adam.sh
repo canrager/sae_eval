4c3eb86dec581b4312bb3c1f14bb2eaf5a802dc0
Adam Karvonen <adam.karvonen@gmail.com>
2024-07-30 03:39:25 +0000

    Fix python file name in shell script

--- train_saes/run_pythia_sae_trainer_adam.sh
+++ train_saes/run_pythia_sae_trainer_adam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 pythia_sae_trainer.py \
+python3 pythia_sae_trainer_adam.py \
     --save_dir /workspace/sae_eval/dictionary_learning/dictionaries/pythia70m_sweep_standard_ctx128_0712 \
     --layers 0 1 2 \
     # --no_wandb_logging \
