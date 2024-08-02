82dca72e418787d998c1f3c594c3663fd9ee2c92
Adam Karvonen <adam.karvonen@gmail.com>
2024-08-02 15:36:17 -0500

    Use config.json instead of ae.pt for compatibility with results folders

--- experiments/utils.py
+++ experiments/utils.py
@@ -67,7 +67,6 @@ class ModelEvalConfig:
 def get_ae_group_paths(
     dictionaries_path: str, sweep_name: str, submodule_trainers: Optional[dict]
 ) -> list[str]:
-
     if submodule_trainers is None:
         return [f"{dictionaries_path}/{sweep_name}"]
 
@@ -121,8 +120,9 @@ def get_nested_folders(path: str) -> list[str]:
     """
     folder_names = []
 
+    # We use config.json so it also works for data folders
     for root, dirs, files in os.walk(path):
-        if "ae.pt" in files:
+        if "config.json" in files:
             folder_names.append(root + "/")
 
     return folder_names
