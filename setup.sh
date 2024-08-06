e6f273f2aaf1fa9ddd590510a627858e0b48b120
Adam Karvonen <adam.karvonen@gmail.com>
2024-08-06 22:01:22 +0000

    Easily download multiple trainer classes

--- setup.sh
+++ setup.sh
@@ -1,26 +1,52 @@
+#!/bin/bash
+
+# Function to download and extract files from HuggingFace
+download_and_extract() {
+    local repo_name=$1
+    local filename=$2
+    local url="https://huggingface.co/${repo_name}/resolve/main/${filename}?download=true"
+    
+    echo "Downloading ${filename}..."
+    wget -O "${filename}" "${url}"
+    
+    echo "Extracting ${filename}..."
+    unzip "${filename}"
+    
+    echo "Removing ${filename}..."
+    rm "${filename}"
+}
+
+# Install requirements
 pip install -r requirements.txt
 pip install -e .
 git submodule update --init
 
-apt install unzip
-apt install zip
+# Install necessary tools
+apt-get update
+apt-get install -y unzip zip gcc
 
+# Navigate to the dictionaries directory
 cd dictionary_learning
-mkdir dictionaries
+mkdir -p dictionaries
 cd dictionaries
 
-wget -O pythia70m_sweep_standard_ctx128_0712.zip "https://huggingface.co/canrager/lm_sae/resolve/main/pythia70m_sweep_standard_ctx128_0712.zip?download=true"
-unzip pythia70m_sweep_standard_ctx128_0712.zip
+# HuggingFace repository name
+REPO_NAME="canrager/lm_sae"
 
+# List of files to download
+FILES=(
+    "pythia70m_sweep_standard_ctx128_0712.zip"
+    "pythia70m_test_sae.zip"
+    "pythia70m_sweep_topk_ctx128_0730.zip"
+    "pythia70m_sweep_gated_ctx128_0730.zip"
+)
 
-wget -O pythia70m_test_sae.zip "https://huggingface.co/canrager/lm_sae/resolve/main/pythia70m_test_sae.zip?download=true"
-unzip pythia70m_test_sae.zip
+# Download and extract each file
+for file in "${FILES[@]}"; do
+    download_and_extract "$REPO_NAME" "$file"
+done
 
-
-
-# For Triton kernels on Vast AI machines
-sudo apt-get update
-sudo apt-get install gcc
+# Set up Triton kernels for Vast AI machines
 export CC=/usr/bin/gcc
 gcc --version
 echo $CC
