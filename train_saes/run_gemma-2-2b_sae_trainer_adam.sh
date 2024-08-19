96c5a543243c77bab2a4a1b9ed1e87507e660241
Adam Karvonen <adam.karvonen@gmail.com>
2024-08-19 02:08:48 +0000

    Finish rename

--- train_saes/run_gemma-2-2b_sae_trainer_adam.sh
+++ train_saes/run_gemma-2-2b_sae_trainer_adam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 gemma2b_sae_trainer_adam.py \
+python3 gemma-2-2b_sae_trainer_adam.py \
     --save_dir /workspace/sae_eval/dictionary_learning/dictionaries/gemma-2-2b_sweep_topk_ctx128_0817 \
     --layers 12 16 20 \
     # --no_wandb_logging \
