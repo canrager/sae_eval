613bca23e02aee2617c169ce01da4680a0861d0b
Adam Karvonen <adam.karvonen@gmail.com>
2024-09-03 19:13:50 +0000

    Fix weird jumprelu errors

--- attribution.py
+++ attribution.py
@@ -39,7 +39,12 @@ def _pe_attrib(
             if is_tuple[submodule]:
                 x = x[0]
             x = x.to(dtype=model.dtype)
-            x_hat, f = dictionary(x, output_features=True)  # x_hat implicitly depends on f
+
+            # I have no idea why (x - 0.0) is necessary
+            # Without it, we get this error from the JumpReluAutoEncoder class
+            # RuntimeError: one of the variables needed for gradient computation has been modified by an inplace operation
+            # It can also be fixed by setting pre_jump = self.encoder(x - 0.0) + self.b_enc in the JumpReluAutoEncoder class
+            x_hat, f = dictionary((x - 0.0), output_features=True)  # x_hat implicitly depends on f
             residual = x - x_hat
             hidden_states_clean[submodule] = SparseAct(act=f, res=residual).save()
             grads[submodule] = hidden_states_clean[submodule].grad.save()
