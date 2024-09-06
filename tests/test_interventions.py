939c265b194782f2d89d1bb3485a5e984e53243d
Adam Karvonen <adam.karvonen@gmail.com>
2024-09-06 21:10:44 +0000

    Add tests for both spurious correlations and tpp

--- tests/test_interventions.py
+++ tests/test_interventions.py
@@ -34,7 +34,7 @@ def compare_dicts_within_tolerance(actual, expected, tolerance, path=""):
         assert actual == expected, f"Value mismatch at {path}: {actual} != {expected}"
 
 
-def test_run_interventions():
+def test_run_interventions_spurious_correlation():
     test_config = PipelineConfig()
 
     test_config.probe_train_set_size = 4000
@@ -80,7 +80,54 @@ def test_run_interventions():
             class_accuracies = pickle.load(f)
         tolerance = 0.03
 
-        with open("tests/test_data/class_accuracies_attrib.pkl", "rb") as f:
+        with open("tests/test_data/class_accuracies_attrib_spurious.pkl", "rb") as f:
+            expected_results = pickle.load(f)
+
+        compare_dicts_within_tolerance(class_accuracies, expected_results, tolerance)
+
+
+def test_run_interventions_tpp():
+    test_config = PipelineConfig()
+
+    test_config.probe_train_set_size = 4000
+    test_config.probe_test_set_size = 1000
+
+    # Load datset and probes
+    test_config.train_set_size = 500
+    test_config.test_set_size = 500
+
+    seed = 42
+
+    test_config.chosen_class_indices = [0, 1, 2]
+
+    test_config.attrib_t_effects = [20]
+
+    test_config.dictionaries_path = "dictionary_learning/dictionaries"
+    test_config.probes_dir = "experiments/trained_bib_probes"
+
+    ae_sweep_paths = {"pythia70m_test_sae": {"resid_post_layer_3": {"trainer_ids": [0]}}}
+
+    for sweep_name, submodule_trainers in ae_sweep_paths.items():
+        bib_intervention.run_interventions(
+            submodule_trainers,
+            test_config,
+            sweep_name,
+            seed,
+            verbose=True,
+        )
+
+        ae_group_paths = utils.get_ae_group_paths(
+            test_config.dictionaries_path, sweep_name, submodule_trainers
+        )
+        ae_paths = utils.get_ae_paths(ae_group_paths)
+
+        output_filename = f"{ae_paths[0]}/class_accuracies_attrib.pkl"
+
+        with open(output_filename, "rb") as f:
+            class_accuracies = pickle.load(f)
+        tolerance = 0.03
+
+        with open("tests/test_data/class_accuracies_attrib_tpp.pkl", "rb") as f:
             expected_results = pickle.load(f)
 
         compare_dicts_within_tolerance(class_accuracies, expected_results, tolerance)
