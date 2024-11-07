7856861ffbe3558a05778e0564418c3f4e0a34e0
Can Rager <web.rager@posteo.de>
2024-11-07 09:31:09 -0500

    adapted training hyperparameters

--- sae_training_can/run_training_sweep.sh
+++ sae_training_can/run_training_sweep.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Define arrays for different configurations
-ARCH="vanilla topk"
-LAYERS=(5 12 19)
-WIDTH=12
-NUM_TOKENS=200_000_000
-DEVICES=("cuda:0" "cuda:1" "cuda:2")
+ARCH=("vanilla" "vanilla" "vanilla" "topk" "topk" "topk")
+LAYERS=(5 12 19 5 12 19)
+WIDTH=14
+NUM_TOKENS=400_000_000
+DEVICES=("cuda:0" "cuda:1" "cuda:2" "cuda:3" "cuda:4" "cuda:5")
 
 # Loop through the configurations
-for i in {0..2}; do
+for i in {0..5}; do
     nohup python3 training_sweep.py \
         --save_dir saes \
-        --architecture ${ARCH} \
+        --architecture ${ARCH[$i]} \
         --layers ${LAYERS[$i]} \
         --width_exponents ${WIDTH} \
         --num_tokens ${NUM_TOKENS}\
         --device ${DEVICES[$i]} \
-        > "vanilla_topk_l${LAYERS[$i]}_w${WIDTH}_${DEVICE//:/_}.out" 2>&1 &
+        > "${ARCH[$i]}_l${LAYERS[$i]}_w${WIDTH}_${DEVICE//:/_}.out" 2>&1 &
 
         # --no_wandb_logging \
         # --dry_run \
 
-    echo "Started job ${i}/2: ${ARCH} with ${LAYERS[$i]} layers"
+    echo "Started job ${i}/6: ${ARCH[$i]} with ${LAYERS[$i]} layers"
     
     # Optional: add a small delay between job submissions
     sleep 2
